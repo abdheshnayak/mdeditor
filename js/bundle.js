9261ff82211bedfc7a0a206b52d7aa2745d2ccf8
abdheshnayak <abdheshnayak@gmail.com>
2021-03-02 15:19:42 +0530

    up

--- js/bundle.js
+++ js/bundle.js
@@ -9,33 +9,36 @@ function copyToClipboard(element) {
   $temp.val($(element).text()).select();
   document.execCommand("copy");
   $temp.remove();
-  $('.htmlOutputContainer').fadeOut('swing');
+  $(".htmlOutputContainer").fadeOut("swing");
 }
 
-function convertinput(){
-  $('body').removeClass('loaded');
-  $('.htmlOutputContainer').fadeIn('swing');
+function convertinput() {
+  $("body").removeClass("loaded");
+  $(".htmlOutputContainer").fadeIn("swing");
   val = editor.session.getValue();
-  document.getElementById("output").innerHTML =marked(val);;
+  document.getElementById("output").innerHTML = marked(val);
   mermaid.init();
   renderMathInElement(document.body);
-  prefix="<!DOCTYPE html>\n<html lang=\"en\" dir=\"ltr\">\n<head>\n<title>MD Editor | Abdhesh Nayak</title>"+
-  '   <link rel="stylesheet"'+
-  '          href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.1/styles/default.min.css">'+
-  '    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.1/highlight.min.js"><'+
-  '/script>'+
-  "\n<meta name=viewport content=\"width=device-width\" scale=1.0>\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<meta http-equiv=content-type content=\"text/html; charset=utf-8\">\n<link rel=\"stylesheet\" href=\"https://kamla.com.np/css/readme.css\" />\n</head>\n<body class=\"stackedit\">\n<div class=\"stackedit__html\">\n";
-  postfix="\n</div>\n"+
-  "<script>hljs.initHighlightingOnLoad();</"+"script>"+
-  "</body>\n</html>";
+  prefix =
+    '<!DOCTYPE html>\n<html lang="en" dir="ltr">\n<head>\n<title>MD Editor | Abdhesh Nayak</title>' +
+    '   <link rel="stylesheet"' +
+    '          href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.1/styles/default.min.css">' +
+    '    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.1/highlight.min.js"><' +
+    "/script>" +
+    '\n<meta name=viewport content="width=device-width" scale=1.0>\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta http-equiv=content-type content="text/html; charset=utf-8">\n<link rel="stylesheet" href="https://cdn.progman.in/css/readme.css" />\n</head>\n<body class="stackedit">\n<div class="stackedit__html">\n';
+  postfix =
+    "\n</div>\n" +
+    "<script>hljs.initHighlightingOnLoad();</" +
+    "script>" +
+    "</body>\n</html>";
 
-  document.getElementById("htmlOutput").innerHTML = prefix + document.getElementById("output").innerHTML + postfix;
-  $('body').addClass('loaded');
+  document.getElementById("htmlOutput").innerHTML =
+    prefix + document.getElementById("output").innerHTML + postfix;
+  $("body").addClass("loaded");
 }
-function updateOutput(){
-
+function updateOutput() {
   val = editor.session.getValue();
-  document.getElementById("output").innerHTML =marked(val);
+  document.getElementById("output").innerHTML = marked(val);
 
   mermaid.init();
   renderMathInElement(document.body);
@@ -44,37 +47,37 @@ function updateOutput(){
 }
 
 function showOutput(str) {
-  if($('#livecb'). prop("checked") == true){
-      if (str.length == 0) {
-        document.getElementById("output").innerHTML = "";
-        return;
-      } else {
-        updateOutput();
-      }
+  if ($("#livecb").prop("checked") == true) {
+    if (str.length == 0) {
+      document.getElementById("output").innerHTML = "";
+      return;
+    } else {
+      updateOutput();
     }
   }
+}
 
-function LoadDiagrams(){
-  $('body').removeClass('loaded');
-  $.post('src/examples/diagrams.txt', function(data) {
+function LoadDiagrams() {
+  $("body").removeClass("loaded");
+  $.post("src/examples/diagrams.txt", function (data) {
     // $("textarea#input").val(data);
     editor.session.setValue(data);
     updateOutput();
-    $('body').addClass('loaded');
+    $("body").addClass("loaded");
   });
 }
-function Loadmain(){
-  $('body').removeClass('loaded');
-  $.post('src/examples/main.txt', function(data) {
+function Loadmain() {
+  $("body").removeClass("loaded");
+  $.post("src/examples/main.txt", function (data) {
     editor.session.setValue(data);
     // $("textarea#input").val(data);
     updateOutput();
-    $('body').addClass('loaded');
+    $("body").addClass("loaded");
   });
 }
 
-function runupdateOutput(){
-  $('body').removeClass('loaded');
+function runupdateOutput() {
+  $("body").removeClass("loaded");
   updateOutput();
-  $('body').addClass('loaded');
+  $("body").addClass("loaded");
 }
